3a9b391393e83d7aae01e722eee6c9bd43e9517e
Mark <kalmark@mail.ru>
2022-10-10 22:49:35 +0300

    Add Ex_19_Palindrom

---
+++ Ex_19_Palindrom/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int mist = 0;
+while (mist == 0)
+{
+    try
+    {
+        Console.Write("Введите пятизначное число: ");
+        int noun = Convert.ToInt32(Console.ReadLine());
+        if (noun / 10000 != 0)
+        {
+            if (noun/10000 == noun % 10 && (noun /1000) % 10 == (noun%100)/10) Console.WriteLine("Это число - палиндром!");
+            else Console.WriteLine("Это число не является палиндромом!");
+            mist = 1;
+        }
+        else Console.WriteLine("Число не пятизначное!");
+    }
+    catch
+    {
+        Console.WriteLine("Ошибочный ввод!");
+    }
+}
