ca8232bebdf1417bed2fb5297f0a551cde0b85bf
Mark <kalmark@mail.ru>
2022-10-10 23:12:32 +0300

    Add one-digit case

--- Ex_19_Palindrom_star/Program.cs
+++ Ex_19_Palindrom_star/Program.cs
@@ -1,11 +1,15 @@
 ﻿Console.Write("Введите натуральное число: ");
 string noun = Console.ReadLine();
 int n = noun.Length;
-bool a = false;
-for (int i = 0; i < n / 2; i++)
+if (n == 1) Console.WriteLine("Это число - палиндром!");
+else
 {
-    if (noun[i] == noun[n - 1 - i]) a = true;
-    else a = false;
+    bool a = false;
+    for (int i = 0; i < n / 2; i++)
+    {
+        if (noun[i] == noun[n - 1 - i]) a = true;
+        else a = false;
+    }
+    if (a) Console.WriteLine("Это число - палиндром!");
+    else Console.WriteLine("Это число не является палиндромом!");
 }
-if (a) Console.WriteLine("Это число - палиндром!");
-else Console.WriteLine("Это число не является палиндромом!");
