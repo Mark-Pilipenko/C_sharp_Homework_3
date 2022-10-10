dde8938faaa7b9e4e5ee6567334a5d14fadd89d5
Mark <kalmark@mail.ru>
2022-10-10 23:07:35 +0300

    Add Ex_19_Palindrom_star

---
+++ Ex_19_Palindrom_star/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите натуральное число: ");
+string noun = Console.ReadLine();
+int n = noun.Length;
+bool a = false;
+for (int i = 0; i < n / 2; i++)
+{
+    if (noun[i] == noun[n - 1 - i]) a = true;
+    else a = false;
+}
+if (a) Console.WriteLine("Это число - палиндром!");
+else Console.WriteLine("Это число не является палиндромом!");
